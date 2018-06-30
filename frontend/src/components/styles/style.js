83fcc99f59374efda7764a3726a9c995677f33e1
Joseph Ramos <jardix22@gmail.com>
2018-06-29 21:34:54 -0500

    Refactored object stylest to create styled components.

--- frontend/src/components/styles/style.js
+++ frontend/src/components/styles/style.js
@@ -1,3 +1,6 @@
+import styled from 'styled-components'
+import media from '../../styleguide/media'
+
 const styles = (theme) => ({
   appBarHeader: {
     color: theme.palette.primary
@@ -9,6 +12,7 @@ const styles = (theme) => ({
   root: {
     flexGrow: 1,
     marginTop: 0,
+    overflowX: 'hidden'
   },
   icon: {
     marginTop: -4,
@@ -35,36 +39,12 @@ const styles = (theme) => ({
       padding: 8,
       backgroundColor: '#f1f0ea'
   },
-  divider: {
-    textAlign: 'center',
-      display: 'inline-block',
-      marginLeft: '35%',
-      marginBottom: '5%',
-      marginTop: 40,
-      fontSize: 18,
-      paddingBottom: 10,
-      borderBottom: '5px solid black',
-      width: '30%',
-  },
-  mainlist: {
-    textAlign: 'left',
-      marginLeft: '20%'
-  },
-  infoList: {
-    textAlign: 'left',
-      marginLeft: '10%'
-  },
   seclist: {
     textAlign: 'left',
       width: '100%'
   },
   listIconTop: {
     alignItems: 'flex-start'
-  },
-  alignLeftPadding: {
-    textAlign: 'left',
-      padding: 80,
-      paddingTop: 40,
   },
   spacedTop: {
     marginTop: 20
@@ -84,5 +64,69 @@ const styles = (theme) => ({
     color: 'black'
   }
 })
+
+export const MainDivider = styled.div`
+  text-align: center;
+  display: block;
+  font-size: 18px;
+  padding-bottom: 10px;
+  border-bottom: 5px solid black;
+  width: 30%;
+
+  margin-top: 40px;
+  margin-left: auto;
+  margin-bottom: 40px;
+  margin-right: auto;
+
+  ${props => props.left && `
+    margin-right: 18%;
+  `}
+
+  ${media.phone`
+    width: 60%;
+    margin: 20px auto;
+
+    ${props => props.left && `margin-left: auto;`}
+  `}
+`
+
+export const MainList = styled.div`
+  text-align: left;
+  margin-left: 20%;
+
+  ${media.phone`
+    margin-left: 0;
+  `}
+`
+
+export const ResponsiveImage = styled.img`
+
+  ${media.phone`
+    width: 100%;
+  `}
+`
+
+export const InfoList = styled.div`
+  text-align: left;
+  margin-left: 10%;
+
+  ${media.phone`
+    margin-left: 0;
+  `}
+`
+export const FooterContainer = styled.div`
+  text-align: left;
+  padding: 80px;
+  padding-top: 40px;
+
+  ${media.phone`
+    text-align: center;
+    padding: 25px 10px 5px 10px;
+
+    a {
+      text-align: center;
+    }
+  `}
+`
 
 export default styles;
